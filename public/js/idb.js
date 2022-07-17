997fa7b305b5cc3461d5aca9e6b92a3790b95b40
Blake Smethers <96027095+smethersblake@users.noreply.github.com>
2022-07-17 11:28:03 -0500

    rearanged files

---
+++ public/js/idb.js
@@ -0,0 +1,57 @@
+const { response } = require("express")
+
+let db
+const request = indexedDB.open("budget_tracker", 1)
+
+const transaction = db.transaction(["new_transaction"], "readwrite")
+const store = transaction.objectStore("new_transaction")
+const getAll = store.getAll()
+
+request.onupgradeneeded = function (event)
+{
+    const db = event.target.result
+    db.createObjectStore("new_transaction", { autoIncrement: true })
+}
+request.onsuccess = function (event)
+{
+    db = event.target.result
+    if (navigator.onLine)
+    {
+        uploadBudget()
+    }
+}
+request.onerror = function (event)
+{
+    console.log("Error Code: " + event.target.errorCode)
+}
+function uploadBudget ()
+{
+    getAll.onsuccess = function ()
+    {
+        if (getAll.result.length > 0)
+        {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, teaxt/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+                .then(response =>
+                {
+                    store.clear()
+            })
+        }
+    }
+}
+function saveBudget (save)
+{
+    store.add(save)
+}
+function deleteTrasaction ()
+{
+    store.clear()
+}
+window.addEventListener("online", uploadBudget)
